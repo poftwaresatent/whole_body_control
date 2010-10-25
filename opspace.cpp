5e1c976fff524919e4a546119b75af992f8a8a27
Roland Philippsen <roland dot philippsen at gmx dot net>
2010-10-24 18:06:27 -0700

    Draft cmake setup and bare-bones operational space
    functions. Hardcoded paths for the jspace and tao libraries from
    stanford-wbc.sf.net (they are assumed to lie underneath
    ../stanford-wbc, with the binaries in ../stanford-wbc/build). It
    compiles... let's build a unit test.

---
+++ opspace.cpp
@@ -0,0 +1,111 @@
+/*
+ * Whole-Body Control for Human-Centered Robotics http://www.me.utexas.edu/~hcrl/
+ *
+ * Copyright (c) 2010 University of Texas at Austin. All rights reserved.
+ *
+ * Authors: Roland Philippsen and Luis Sentis
+ *
+ * BSD license:
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ * 3. Neither the name of the copyright holder nor the names of
+ *    contributors to this software may be used to endorse or promote
+ *    products derived from this software without specific prior written
+ *    permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE AUTHORS AND CONTRIBUTORS ``AS IS''
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED
+ * TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A
+ * PARTICULAR PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL THE COPYRIGHT
+ * HOLDER OR THE CONTRIBUTORS TO THIS SOFTWARE BE LIABLE FOR ANY
+ * DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE
+ * GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
+ * WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+ * NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#include <jspace/Model.hpp>
+#include <Eigen/LU>
+#include <Eigen/SVD>
+
+using namespace std;
+
+
+void pseudoInverse(jspace::Matrix const & matrix,
+		   double sigmaThreshold,
+		   jspace::Matrix & invMatrix)
+{
+  Eigen::SVD<jspace::Matrix> svd(matrix);
+  // not sure if we need to svd.sort()... probably not
+  int const nrows(svd.singularValues().rows());
+  jspace::Matrix invS;
+  invS = jspace::Matrix::Zero(nrows, nrows);
+  for (int ii(0); ii < nrows; ++ii) {
+    if (svd.singularValues().coeff(ii) > sigmaThreshold) {
+      invS.coeffRef(ii, ii) = 1.0 / svd.singularValues().coeff(ii);
+    }
+  }
+  invMatrix = svd.matrixU() * invS * svd.matrixU().transpose();
+}
+
+
+void computeLambda(jspace::Matrix const * nullspace_in,
+		   jspace::Matrix const * jacobian,
+		   jspace::Matrix const & invMassInertia,
+		   jspace::Matrix & lambda,
+		   jspace::Matrix * nullspace_out)
+{
+  if (nullspace_in) {
+    if (jacobian) {
+      jspace::Matrix const
+	invLambda((*nullspace_in) * (*jacobian) * invMassInertia * jacobian->transpose());
+      pseudoInverse(invLambda, 1e-3, lambda);
+    }
+    else {
+      jspace::Matrix const invLambda((*nullspace_in) * invMassInertia);
+      pseudoInverse(invLambda, 1e-3, lambda);
+    }
+  }
+  else {
+    if (jacobian) {
+      jspace::Matrix const invLambda((*jacobian) * invMassInertia * jacobian->transpose());
+      pseudoInverse(invLambda, 1e-3, lambda);
+    }
+    else {
+      // Well, actually in this case the caller should just directly
+      // use massInertia from jspace::Model which avoids the explicit
+      // matrix inversion.
+      lambda = invMassInertia.inverse();
+    }
+  }
+  
+  if (nullspace_out) {
+    if (jacobian) {
+      jspace::Matrix const jbar(invMassInertia * jacobian->transpose() * lambda);
+      int nrows(jbar.rows());
+      *nullspace_out = jspace::Matrix::Identity(nrows, nrows) - jbar * (*jacobian);
+    }
+    else {
+      // Jacobian is identity
+      if (nullspace_in) {
+	jspace::Matrix const jbar(invMassInertia * lambda);
+	int nrows(jbar.rows());
+	*nullspace_out = jspace::Matrix::Identity(nrows, nrows) - jbar;
+      }
+      else {
+	// Lambda is massInertia... and Jacobian is identity, so Jbar is identity
+	int nrows(invMassInertia.rows());
+	*nullspace_out = jspace::Matrix::Zero(nrows, nrows);
+      }
+    }
+  }
+}
