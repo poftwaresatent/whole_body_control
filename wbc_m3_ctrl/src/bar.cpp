7b1e6c998a7c31b2edf55e3e110243a777d94d0a
Roland Philippsen <poftwaresatent@gmail.com>
2011-03-04 18:03:59 -0800

    Let's put the Meka arm controllers into the wbc_m3_ctrl ROS package. It'll have to detect whether we are on an RTAI machine with the appropriate M3 development setup, so that might get hairy, but essentially it will be a copy of the executables in https://github.com/poftwaresatent/utaustin-wbc with a bit of ROS frosting.

---
+++ wbc_m3_ctrl/src/bar.cpp
@@ -0,0 +1,7 @@
+#include <wbc_m3_ctrl/foo.hpp>
+#include <stdio.h>
+
+int main(int argc, char ** argv)
+{
+  printf("foo(42) is %d\n", foo(42));
+}
---
+++ wbc_m3_ctrl/src/foo.cpp
@@ -0,0 +1,6 @@
+#include <wbc_m3_ctrl/foo.hpp>
+
+int foo(int blah)
+{
+  return 2 * blah;
+}
