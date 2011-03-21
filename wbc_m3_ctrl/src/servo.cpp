1e38b007cbb7db872696587860be5ab22808e4d9
Roland Philippsen <poftwaresatent@gmail.com>
2011-03-21 16:07:38 -0700

    First implementation of servo that is ROS and RTAI compatible... let's see if it runs.

---
+++ wbc_m3_ctrl/src/servo.cpp
@@ -0,0 +1,342 @@
+/*
+ * Whole-Body Control for Human-Centered Robotics http://www.me.utexas.edu/~hcrl/
+ *
+ * Copyright (c) 2011 University of Texas at Austin. All rights reserved.
+ *
+ * Author: Roland Philippsen
+ *
+ * BSD license:
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ * 3. Neither the name of the copyright holder nor the names of
+ *    contributors to this software may be used to endorse or promote
+ *    products derived from this software without specific prior written
+ *    permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE AUTHORS AND CONTRIBUTORS ``AS IS''
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED
+ * TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A
+ * PARTICULAR PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL THE COPYRIGHT
+ * HOLDER OR THE CONTRIBUTORS TO THIS SOFTWARE BE LIABLE FOR ANY
+ * DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE
+ * GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
+ * WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+ * NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#include <wbc_m3_ctrl/rt_util.h>
+
+#include <ros/ros.h>
+#include <jspace/test/sai_util.hpp>
+#include <opspace/Behavior.hpp>
+#include <opspace/Factory.hpp>
+#include <opspace/ControllerNG.hpp>
+#include <boost/scoped_ptr.hpp>
+#include <err.h>
+#include <signal.h>
+
+using namespace wbc_m3_ctrl;
+using namespace opspace;
+using namespace boost;
+using namespace std;
+
+
+static char const * opspace_fallback_str = 
+  "- tasks:\n"
+  "  - type: opspace::PositionTask\n"
+  "    name: eepos\n"
+  "    end_effector_id: 6\n"
+  "    dt_seconds: 0.002\n"
+  "    kp: [ 100.0 ]\n"
+  "    kd: [  20.0 ]\n"
+  "    maxvel: [ 0.5 ]\n"
+  "    maxacc: [ 1.5 ]\n"
+  "  - type: opspace::PostureTask\n"
+  "    name: posture\n"
+  "    dt_seconds: 0.002\n"
+  "    kp: [ 100.0 ]\n"
+  "    kd: [  20.0 ]\n"
+  "    maxvel: [ 3.1416 ]\n"
+  "    maxacc: [ 6.2832 ]\n"
+  "- behaviors:\n"
+  "  - type: opspace::TPBehavior\n"
+  "    name: task_posture\n"
+  "    default:\n"
+  "      eepos: eepos\n"
+  "      posture: posture\n";
+
+
+static bool verbose(false);
+static scoped_ptr<jspace::Model> model;
+static Factory factory;
+static long long servo_rate;
+
+
+static void usage(int ecode, std::string msg)
+{
+  errx(ecode,
+       "%s\n"
+       "  options:\n"
+       "  -h               help (this message)\n"
+       "  -v               verbose mode\n"
+       "  -r  <filename>   robot specification (SAI XML format)\n"
+       "  -f  <frequency>  servo rate (integer number in Hz, default 500Hz)\n"
+       "  -s  <filename>   skill specification (YAML file with tasks etc)",
+       msg.c_str());
+}
+
+
+static void parse_options(int argc, char ** argv)
+{
+  string skill_spec("");
+  string robot_spec("");
+  servo_rate = 500;
+  
+  for (int ii(1); ii < argc; ++ii) {
+    if ((strlen(argv[ii]) < 2) || ('-' != argv[ii][0])) {
+      usage(EXIT_FAILURE, "problem with option `" + string(argv[ii]) + "'");
+    }
+    else
+      switch (argv[ii][1]) {
+	
+      case 'h':
+	usage(EXIT_SUCCESS, "servo [-h] [-v] [-s skillspec] -r robotspec");
+	
+      case 'v':
+	verbose = true;
+ 	break;
+	
+      case 'r':
+ 	++ii;
+ 	if (ii >= argc) {
+	  usage(EXIT_FAILURE, "-r requires parameter");
+ 	}
+	robot_spec = argv[ii];
+ 	break;
+	
+      case 'f':
+ 	++ii;
+ 	if (ii >= argc) {
+	  usage(EXIT_FAILURE, "-f requires parameter");
+ 	}
+	else {
+	  istringstream is(argv[ii]);
+	  is >> servo_rate;
+	  if ( ! is) {
+	    usage(EXIT_FAILURE, "failed to read servo rate from `" + string(argv[ii]) + "'");
+	  }
+	  if (0 >= servo_rate) {
+	    usage(EXIT_FAILURE, "servo rate has to be positive");
+	  }
+	}
+ 	break;
+	
+      case 's':
+ 	++ii;
+ 	if (ii >= argc) {
+	  usage(EXIT_FAILURE, "-s requires parameter");
+ 	}
+	skill_spec = argv[ii];
+ 	break;
+	
+      default:
+	usage(EXIT_FAILURE, "invalid option `" + string(argv[ii]) + "'");
+      }
+  }
+  
+  try {
+    if (robot_spec.empty()) {
+      usage(EXIT_FAILURE, "no robot specification (see option -r)");
+    }
+    if (verbose) {
+      warnx("reading robot spec from %s", robot_spec.c_str());
+    }
+    static bool const enable_coriolis_centrifugal(false);
+    model.reset(jspace::test::parse_sai_xml_file(robot_spec, enable_coriolis_centrifugal));
+  }
+  catch (runtime_error const & ee) {
+    errx(EXIT_FAILURE,
+	 "exception while parsing robot specification\n"
+	 "  filename: %s\n"
+	 "  error: %s",
+	 robot_spec.c_str(), ee.what());
+  }
+  
+  Status st;
+  if (skill_spec.empty()) {
+    if (verbose) {
+      warnx("using fallback task/posture skill");
+    }
+    st = factory.parseString(opspace_fallback_str);
+  }
+  else {
+    if (verbose) {
+      warnx("reading skills from %s", skill_spec.c_str());
+    }
+    st = factory.parseFile(skill_spec);
+  }
+  if ( ! st) {
+    errx(EXIT_FAILURE,
+	 "failed to parse skills\n"
+	 "  specification file: %s\n"
+	 "  error description: %s",
+	 skill_spec.c_str(), st.errstr.c_str());
+  }
+  if (verbose) {
+    factory.dump(cout, "*** parsed tasks and skills", "* ");
+  }
+}
+
+
+static void handle(int signum)
+{
+  if (ros::ok()) {
+    warnx("caught signal, requesting shutdown");
+    ros::shutdown();
+  }
+  else {
+    errx(EXIT_SUCCESS, "caught signal (again?), attempting forced exit");
+  }
+}
+
+
+namespace {
+  
+  
+  class Servo
+    : public RTUtil
+  {
+  public:
+    shared_ptr<ControllerNG> controller;
+    shared_ptr<Behavior> skill;
+    
+    
+    virtual int init(jspace::State const & state) {
+      if (controller) {
+	warnx("Servo::init(): already initialized");
+	return -1;
+      }
+      if (factory.getBehaviorTable().empty()) {
+	warnx("Servo::init(): empty skill table");
+	return -2;
+      }
+      if ( ! model) {
+	warnx("Servo::init(): no model");
+	return -3;
+      }
+      
+      model->update(state);
+    
+      controller.reset(new ControllerNG("wbc_m3_ctrl::servo"));
+      jspace::Status status(controller->init(*model));
+      if ( ! status) {
+	warnx("Servo::init(): controller->init() failed: %s", status.errstr.c_str());
+	controller.reset();
+	return -4;
+      }
+      
+      skill = factory.getBehaviorTable()[0]; // XXXX to do: allow selection at runtime
+      status = skill->init(*model);
+      if ( ! status) {
+	warnx("Servo::init(): skill->init() failed: %s", status.errstr.c_str());
+	skill.reset();
+	controller.reset();
+	return -5;
+      }
+      
+      return 0;
+    }
+    
+    
+    virtual int update(jspace::State const & state,
+		       jspace::Vector & command)
+    {
+      if ( ! controller) {
+	warnx("Servo::update(): not initialized\n");
+	return -1;
+      }
+      
+      model->update(state);
+      
+      jspace::Status status(controller->computeCommand(*model, *skill, command));
+      if ( ! status) {
+	warnx("Servo::update(): controller->computeCommand() failed: %s", status.errstr.c_str());
+	return -2;
+      }
+      
+      return 0;
+    }
+    
+    
+    virtual int cleanup(void)
+    {
+      skill.reset();
+      controller.reset();
+      return 0;
+    }
+    
+    
+    virtual int slowdown(long long iteration,
+			 long long desired_ns,
+			 long long actual_ns)
+    {
+      if (iteration > 3) {
+	warnx("Servo::slowdown(): I'm afraid I can't let you do that");
+	return -1;
+	
+      }
+      return 0;
+    }
+  };
+  
+}
+
+
+int main(int argc, char ** argv)
+{
+  struct sigaction sa;
+  bzero(&sa, sizeof(sa));
+  sa.sa_handler = handle;
+  if (0 != sigaction(SIGINT, &sa, 0)) {
+    err(EXIT_FAILURE, "sigaction");
+  }
+  
+  ros::init(argc, argv, "wbc_m3_ctrl_servo", ros::init_options::NoSigintHandler);
+  parse_options(argc, argv);
+  
+  Servo servo;
+  try {
+    if (verbose) {
+      warnx("starting servo with %lld Hz", servo_rate);
+    }
+    servo.start(servo_rate);
+  }
+  catch (std::runtime_error const & ee) {
+    errx(EXIT_FAILURE, "failed to start servo: %s", ee.what());
+  }
+  
+  warnx("started servo RT thread");
+  
+  while (ros::ok()) {
+    // servo has successfully spawned RT thread, just do some
+    // debugging etc here
+    if (verbose) {
+      servo.skill->dbg(cout, "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~", "");
+      servo.controller->dbg(cout, "--------------------------------------------------", "");
+    }
+    ros::spinOnce();
+    usleep(250000);
+  }
+  
+  warnx("shutting down");
+  servo.shutdown();
+}
